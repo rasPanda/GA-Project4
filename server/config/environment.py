deb4662867e0a62468e2460e8e2720bf84dff6f7
raspanda <carlviberg@gmail.com>
2021-03-17 12:28:10 +0000

    seeding debug

--- server/config/environment.py
+++ server/config/environment.py
@@ -1,4 +1,4 @@
 import os
 
-db_URI = os.getenv('DATABASE_URL', 'postgres://localhost:5432/interiorlist_db')
-secret = os.getenv('SECRET', 'Anybody Grudging Twisted Satiable Strongman Sprinkled')
+db_URI = os.getenv("DATABASE_URL", "postgresql://localhost:5432/interiorlist_db")
+secret = os.getenv("SECRET", "Anybody Grudging Twisted Satiable Strongman Sprinkled")
