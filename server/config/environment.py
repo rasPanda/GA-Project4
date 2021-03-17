1717d1f4b6c132b2b2420a07e6e9c29fd904bbc2
raspanda <carlviberg@gmail.com>
2021-03-17 13:02:40 +0000

    deployment debugging

--- server/config/environment.py
+++ server/config/environment.py
@@ -1,4 +1,7 @@
 import os
 
-db_URI = os.getenv("DATABASE_URL", "postgres://localhost:5432/interiorlist_db")
+db_URI = os.getenv("DATABASE_URL", "postgresql://localhost:5432/interiorlist_db")
 secret = os.getenv("SECRET", "Anybody Grudging Twisted Satiable Strongman Sprinkled")
+
+# db_URI = "postgresql://localhost:5432/interiorlist_db"
+# secret = "Anybody Grudging Twisted Satiable Strongman Sprinkled"
